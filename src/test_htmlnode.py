3214c82f407c0ea6fd1a02663165c926c5643524
deep123845 <deep123845@gmail.com>
2026-01-19 16:24:36 -0500

    added leaf node class and tests

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import *
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -8,14 +8,30 @@ class TestHTMLNode(unittest.TestCase):
         node = HTMLNode(None, None, None, {"href": "www.google.com"})
         props = ' href="www.google.com"'
         self.assertEqual(node.props_to_html(), props)
+
     def test_props2(self):
-        node = HTMLNode(None, None, None, {"href": "www.google.com", "target": "_blank"})
+        node = HTMLNode(
+            None, None, None, {"href": "www.google.com", "target": "_blank"}
+        )
         props = ' href="www.google.com" target="_blank"'
         self.assertEqual(node.props_to_html(), props)
+
     def test_props3(self):
         node = HTMLNode(None, None, None, None)
-        props = "" 
+        props = ""
         self.assertEqual(node.props_to_html(), props)
+
+    def test_leaf_to_html_raw(self):
+        node = LeafNode(None, "Hello, world!")
+        self.assertEqual(node.to_html(), "Hello, world!")
+
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_to_html_a(self):
+        node = LeafNode("a", "Google", {"href": "https://www.google.com"})
+        self.assertEqual(node.to_html(), '<a href="https://www.google.com">Google</a>')
 
 
 if __name__ == "__main__":
