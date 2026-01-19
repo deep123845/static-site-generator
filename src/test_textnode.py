80b6280d2b7716c9f1220517129142c720b07bf4
deep123845 <deep123845@gmail.com>
2026-01-19 16:49:21 -0500

    added conversion function from text to html node

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import *
 
 
 class TestTextNode(unittest.TestCase):
@@ -8,15 +8,22 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is a text node", TextType.Bold)
         node2 = TextNode("This is a text node", TextType.Bold)
         self.assertEqual(node, node2)
+
     def test_not_eq(self):
         node = TextNode("This is a text node", TextType.Italic)
         node2 = TextNode("This is a different text node", TextType.Italic)
         self.assertNotEqual(node, node2)
+
     def test_url_not_eq(self):
         node = TextNode("This is a text node", TextType.Link)
         node2 = TextNode("This is a text node", TextType.Link, "Test")
         self.assertNotEqual(node, node2)
 
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.Plain)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
 
 
 if __name__ == "__main__":
--- src/textnode.py
+++ src/textnode.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from htmlnode import *
 
 
 class TextType(Enum):
@@ -10,14 +11,36 @@ class TextType(Enum):
     Image = "image"
 
 
-class TextNode():
+class TextNode:
     def __init__(self, text, text_type, url=None):
         self.text = text
         self.text_type = text_type
         self.url = url
-    
+
     def __eq__(self, other):
-        return (self.text == other.text) and (self.text_type == other.text_type) and (self.url == other.url)
-    
+        return (
+            (self.text == other.text)
+            and (self.text_type == other.text_type)
+            and (self.url == other.url)
+        )
+
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+
+
+def text_node_to_html_node(text_node):
+    match (text_node.text_type):
+        case TextType.Plain:
+            return LeafNode(None, text_node.text)
+        case TextType.Bold:
+            return LeafNode("b", text_node.text)
+        case TextType.Italic:
+            return LeafNode("i", text_node.text)
+        case TextType.Code:
+            return LeafNode("code", text_node.text)
+        case TextType.Link:
+            return LeafNode("a", text_node.text, {"href": text_node.url})
+        case TextType.Image:
+            return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
+        case _:
+            raise Exception("Error, Invalid Text Type")
