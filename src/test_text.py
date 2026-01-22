2c4f8e4a179a162c5047a6564fb6033a2a8b3985
deep123845 <deep123845@gmail.com>
2026-01-21 23:12:35 -0500

    added extracting links and images from markdown + tests

--- src/test_text.py
+++ src/test_text.py
@@ -83,6 +83,24 @@ class TestInlineMarkdown(unittest.TestCase):
             new_nodes,
         )
 
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+    def test_extract_markdown_links(self):
+        matches = extract_markdown_links(
+            "This is text with a [link](https://boot.dev) and [another link](https://blog.boot.dev)"
+        )
+        self.assertListEqual(
+            [
+                ("link", "https://boot.dev"),
+                ("another link", "https://blog.boot.dev"),
+            ],
+            matches,
+        )
+
 
 if __name__ == "__main__":
     unittest.main()
--- src/text.py
+++ src/text.py
@@ -1,4 +1,5 @@
 from textnode import *
+import re
 
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
@@ -19,3 +20,13 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
                 new_nodes.append(TextNode(text_segments[i], text_type))
 
     return new_nodes
+
+
+def extract_markdown_images(text):
+    matches = re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+    return matches
+
+
+def extract_markdown_links(text):
+    matches = re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+    return matches
