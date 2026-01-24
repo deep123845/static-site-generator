db710892a889942e7c0631ea01fdd607a4c4fa80
deep123845 <deep123845@gmail.com>
2026-01-24 15:34:04 -0500

    added static folder and function to copy static contents into public

--- src/main.py
+++ src/main.py
@@ -1,15 +1,33 @@
 from textnode import *
 from htmlnode import *
 from text import *
+import shutil
+import os
+
+
+def recursive_copy_static():
+    if os.path.exists("public"):
+        shutil.rmtree("public")
+    os.mkdir("public")
+
+    recursive_copy("static")
+
+
+def recursive_copy(path):
+    items = os.listdir(path)
+
+    for item in items:
+        item_path = os.path.join(path, item)
+        new_path = item_path.replace("static", "public", 1)
+        if os.path.isfile(item_path):
+            shutil.copy(item_path, new_path)
+        else:
+            os.mkdir(new_path)
+            recursive_copy(item_path)
 
 
 def main():
-    node = TextNode(
-        "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
-        TextType.Plain,
-    )
-    nodes = split_nodes_image([node])
-    print(nodes)
+    recursive_copy_static()
 
 
 main()
