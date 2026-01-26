5e6feece83b3d74010fc8da50763665c68970b14
deep123845 <deep123845@gmail.com>
2026-01-26 10:57:04 -0500

    added support for different basepaths

--- src/main.py
+++ src/main.py
@@ -4,14 +4,15 @@ from text import *
 import shutil
 import os
 from pagegen import *
+import sys
 
 
-def recursive_copy_static():
-    if os.path.exists("public"):
-        shutil.rmtree("public")
-    os.mkdir("public")
+def recursive_copy_static(static_path, public_path):
+    if os.path.exists(public_path):
+        shutil.rmtree(public_path)
+    os.mkdir(public_path)
 
-    recursive_copy("static")
+    recursive_copy(static_path)
 
 
 def recursive_copy(path):
@@ -27,27 +28,31 @@ def recursive_copy(path):
             recursive_copy(item_path)
 
 
-def generate_pages(path):
-    template = "template.html"
-    items = os.listdir(path)
+def generate_pages(content_path, template_path):
+    items = os.listdir(content_path)
 
     for item in items:
-        item_path = os.path.join(path, item)
+        item_path = os.path.join(content_path, item)
         new_path = item_path.replace("content", "public", 1)
         new_path = new_path.rsplit(".md", 1)
         new_path = ".html".join(new_path)
         if os.path.isfile(item_path):
             if item[-3:] != ".md":
                 continue
-            generate_page(item_path, template, new_path)
+            generate_page(item_path, template_path, new_path)
         else:
             os.mkdir(new_path)
-            generate_pages(item_path)
+            generate_pages(item_path, template_path)
 
 
 def main():
-    recursive_copy_static()
-    generate_pages("content")
+    basepath = sys.argv[1]
+    static_path = os.path.join(basepath, "static")
+    content_path = os.path.join(basepath, "content")
+    public_path = os.path.join(basepath, "public")
+    template_path = os.path.join(basepath, "template.html")
+    recursive_copy_static(static_path, public_path)
+    generate_pages(content_path, template_path)
 
 
 main()
